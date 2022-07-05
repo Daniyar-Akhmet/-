84d4836447806bfea2dd1404639fbafe15e1fafe
Daniyar-Akhmet <Daniyar@asitek.kz>
2022-07-05 20:46:02 +0600

    added task_1

---
+++ Akhmetov_Daniyar_dz_1_task_1.py
@@ -0,0 +1,19 @@
+import requests
+import json
+from pprint import pprint
+
+
+result = []
+
+for i in range(1, 10):
+    url = f"https://api.github.com/users/defunkt/repos?page={i}"
+    session = requests.Session()
+    try:
+        response = session.get(url)
+        result.extend([response.json()[i]['archive_url'].split('{', 1)[0].replace('api.', '').replace('repos/', '') for i in range(0, len(response.json()))])
+    except requests.RequestException:
+        print('Error')
+
+result_json = json.dumps(result)
+pprint(result_json)
+print(len(result))
